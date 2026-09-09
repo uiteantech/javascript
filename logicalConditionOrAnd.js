62ecff479a01f6c4cabe7b61e38fef5e2d10becb
Muhammad Usman <uiteanhub@gmail.com>
2026-09-09 21:33:10 +0500

    Add logical AND OR conditions

---
+++ logicalConditionOrAnd.js
@@ -0,0 +1,14 @@
+const age = 25;
+const hasLicense = true;
+
+if (age >= 18 && hasLicense) {
+    console.log("Can drive");
+}
+
+
+const isAdmin = false;
+const isManager = true;
+
+if (isAdmin || isManager) {
+    console.log("Access granted");
+}
